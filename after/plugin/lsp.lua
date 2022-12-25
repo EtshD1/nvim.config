e156e4a69baa410443613c3e0f80426f3d37e172
EtshD1 <mohamed.h.abouelenin@gmail.com>
2022-12-25 15:45:46 +0200

    Add Autocomplete icons (Not Working)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,4 +1,22 @@
 local lsp = require('lsp-zero')
+local cmp = require('cmp')
+local lspkind = require('lspkind')
+
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (entry, vim_item)
+        return vim_item
+      end
+    })
+  }
+}
 lsp.preset('recommended')
 
 lsp.ensure_installed({
@@ -16,7 +34,6 @@ lsp.configure('sumneko_lua', {
     }
 })
 
-local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
--- lua/etsh/plugins.lua
+++ lua/etsh/plugins.lua
@@ -66,5 +66,7 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+    -- Autocompletion Icons
+    use {'onsails/lspkind.nvim'}
 end)
 
