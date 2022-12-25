05c75b913372645dba63e792959b2195b11ae20a
EtshD1 <mohamed.h.abouelenin@gmail.com>
2022-12-25 19:22:50 +0200

    LSP Saga

--- lua/etsh/plugins.lua
+++ lua/etsh/plugins.lua
@@ -20,6 +20,10 @@ vim.cmd([[
 ]])
 
 return require('packer').startup(function(use)
+    -- Typescript
+	use("jose-elias-alvarez/typescript.nvim")
+    -- LSP utils
+    use({ "glepnir/lspsaga.nvim", branch = "main" })
     -- Icons
     use 'nvim-tree/nvim-web-devicons'
 	-- Packer can manage itself
