1537f339a441f2d0486def22dfe655088cf487f8
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-07-16 13:05:09 +0300

    Update which-key config

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,17 +1,26 @@
-local status, which_key = pcall(require, 'which-key')
+local status, which_key = pcall(require, "which-key")
 
 if not status then
-  print("which-key is not found")
-  return
+	print("which-key is not found")
+	return
 end
 
 -- document existing key chains
-which_key.register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+which_key.add({
+	{ "<leader>c", group = "[C]ode" },
+	{ "<leader>c_", hidden = true },
+	{ "<leader>d", group = "[D]ocument" },
+	{ "<leader>d_", hidden = true },
+	{ "<leader>g", group = "[G]it" },
+	{ "<leader>g_", hidden = true },
+	{ "<leader>h", group = "More git" },
+	{ "<leader>h_", hidden = true },
+	{ "<leader>r", group = "[R]ename" },
+	{ "<leader>r_", hidden = true },
+	{ "<leader>s", group = "[S]earch" },
+	{ "<leader>s_", hidden = true },
+	{ "<leader>w", group = "[W]orkspace" },
+	{ "<leader>w_", hidden = true },
+	{ "<leader>b", group = "[B]uffer" },
+	{ "<leader>b_", hidden = true },
+})
