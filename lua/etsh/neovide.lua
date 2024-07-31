b37aef64088b5ed7f776be4e0b741e0fdde4b906
EtshD1 <mohamed.h.abouelenin@gmail.com>
2024-07-31 22:00:57 +0300

    Configure for neovide

--- lua/etsh/neovide.lua
+++ lua/etsh/neovide.lua
@@ -1,2 +1,4 @@
-vim.g.neovide_cursor_vfx_mode = "railgun"
-vim.o.guifont = 'MesloLGS NF'
+if vim.g.neovide then
+	vim.g.neovide_cursor_vfx_mode = "railgun"
+	vim.o.guifont = "MesloLGM Nerd Font"
+end
